5c152ee29e8d58e2b7f64048e0214afb1d385fd6
martip <martip@gmail.com>
2023-03-17 18:50:36 +0100

    Added dynamic credentials; improved error messages

--- google-apis/google-apis.js
+++ google-apis/google-apis.js
@@ -1,7 +1,6 @@
 const { google } = require('googleapis');
 
-module.exports = function(RED) {
-
+module.exports = function (RED) {
   const encodeAPI = (name, version) => {
     return `${name}:${version}`;
   };
@@ -10,18 +9,17 @@ module.exports = function(RED) {
     const a = api.split(':', 2);
     return {
       name: a[0],
-      version: a[1]
+      version: a[1],
     };
   };
 
   const discovery = google.discovery({ version: 'v1' });
 
   RED.httpAdmin.get('/google-apis/list', async (req, res) => {
-
     const list = await discovery.apis.list({
-      fields: 'items(name,version)'
+      fields: 'items(name,version)',
     });
-    const response = list.data.items.map(x => {
+    const response = list.data.items.map((x) => {
       return encodeAPI(x.name, x.version);
     });
     response.sort();
@@ -29,138 +27,179 @@ module.exports = function(RED) {
   });
 
   RED.httpAdmin.get('/google-apis/:api/info', async (req, res) => {
-
     const api = decodeAPI(req.params.api);
 
     try {
       const result = await discovery.apis.getRest({
         api: api.name,
         version: api.version,
-        fields: 'auth,methods,resources'
+        fields: 'auth,methods,resources',
       });
 
       const response = {
         methods: [],
-        scopes: []
+        scopes: [],
       };
-  
+
       const processResources = (d, parent) => {
         const prefix = parent ? parent + '.' : '';
         if (d.methods) {
-          Object.keys(d.methods).forEach(function(k) {
+          Object.keys(d.methods).forEach(function (k) {
             response.methods.push(prefix + k);
           });
         }
         if (d.resources) {
-          Object.keys(d.resources).forEach(function(k) {
+          Object.keys(d.resources).forEach(function (k) {
             processResources(d.resources[k], prefix + k);
           });
         }
-      }
+      };
 
       processResources(result.data);
-  
+
       response.methods.sort();
       response.scopes = Object.keys(result.data.auth.oauth2.scopes);
-  
+
       res.json(response);
-      
     } catch (error) {
       res.status(500).json(error);
     }
-
   });
 
+  function getAuth(key, email, scopes) {
+    try {
+      const auth = new google.auth.JWT({
+        email,
+        key,
+        scopes,
+      });
+      return auth;
+    } catch (error) {
+      throw 'Invalid credentials';
+    }
+  }
 
   function GoogleAPIsAuthenticationNode(config) {
-      let auth = null;
-      RED.nodes.createNode(this, config);
-      this.key = JSON.parse(config.key);
-      this.scopes = config.scopes;
-      this.getAuth = function(scopes) {
-        if(!auth) {
-          auth = new google.auth.JWT({
-            email: this.key.client_email,
-            key: this.key.private_key,
-            scopes: this.scopes.split('\n')
-          });
-        }
-        return auth;
-      };
+    let auth = null;
+    RED.nodes.createNode(this, config);
+    this.key = JSON.parse(config.key);
+    this.scopes = config.scopes;
+    this.getAuth = function (scopes) {
+      if (!auth) {
+        auth = getAuth(
+          this.key.private_key,
+          this.key.client_email,
+          this.scopes.split('\n')
+        );
+      }
+      return auth;
+    };
   }
 
   function GoogleAPIsNode(config) {
-
-      RED.nodes.createNode(this, config);
-      const node = this;
+    RED.nodes.createNode(this, config);
+    const node = this;
+    try {
       node.config = RED.nodes.getNode(config.authentication);
-      node.api = config.api;
-      node.method = config.method;
-      node.scopes = config.scopes;
+    } catch {
+      node.config = null;
+    }
+    node.api = config.api;
+    node.method = config.method;
+    node.scopes = config.scopes;
 
-      node.on('input', function(msg) {
+    node.on('input', function (msg) {
+      node.status({
+        fill: 'blue',
+        shape: 'dot',
+        text: 'pending',
+      });
+
+      let auth;
+      try {
+        if (node.config) {
+          auth = node.config.getAuth();
+        } else {
+          if (msg.google && msg.google.credentials) {
+            const { key, email, scopes } = msg.google.credentials;
+            if (!key) {
+              throw 'Private Key not found. Cannot authenticate with Google.';
+            }
+            if (!email) {
+              throw 'Client Email not found. Cannot authenticate with Google.';
+            }
+            if (!scopes) {
+              throw 'Scopes not found. Cannot authenticate with Google.';
+            }
+            auth = getAuth(key, email, scopes);
+          }
+        }
+      } catch (error) {
+        node.status({
+          fill: 'red',
+          shape: 'dot',
+          text: 'error',
+        });
+        node.error(error);
+        return;
+      }
+
+      let api = decodeAPI(node.api);
+      api = google[api.name]({
+        version: api.version,
+        auth: auth,
+      });
+
+      // if (auth) {
+
+      auth.authorize(function (err, tokens) {
+        if (err) {
+          node.status({
+            fill: 'red',
+            shape: 'dot',
+            text: 'error',
+          });
+          node.error(err);
+          return;
+        }
+
+        const props = node.method.split('.');
+        let method = api;
+        props.forEach(function (val) {
+          method = method[val];
+        });
+
+        const s = method.bind(api);
+        s(msg.payload, { responseType: 'arraybuffer' }, function (err, res) {
+          if (err) {
+            node.status({
+              fill: 'red',
+              shape: 'dot',
+              text: 'error',
+            });
+            const error = JSON.parse(Buffer.from(err.message).toString()).error;
+            node.error(error);
+            return;
+          }
 
           node.status({
-              fill: 'blue',
-              shape: 'dot',
-              text: 'pending'
+            fill: 'yellow',
+            shape: 'dot',
+            text: 'success',
           });
 
-          const auth = node.config.getAuth();
+          msg.payload = Buffer.from(res.data);
 
-          let api = decodeAPI(node.api);
-          api = google[api.name]({
-              version: api.version,
-              auth: auth
-          });
-
-          auth.authorize(function(err, tokens) {
-
-              if (err) {
-                  node.status({
-                      fill: 'red',
-                      shape: 'dot',
-                      text: 'error'
-                  });
-                  node.error(JSON.parse(JSON.stringify(err)));
-                  return;
-              }
-
-              const props = node.method.split('.');
-              let method = api;
-              props.forEach(function(val) {
-                  method = method[val];
-              });
-
-              const s = method.bind(api);
-              s(msg.payload, { responseType: 'arraybuffer' }, function(err, res) {
-
-                  if (err) {
-                      node.status({
-                          fill: 'red',
-                          shape: 'dot',
-                          text: 'error'
-                      });
-                      node.error(JSON.parse(JSON.stringify(err)));
-                      return;
-                  }
-
-                  node.status({
-                      fill: 'yellow',
-                      shape: 'dot',
-                      text: 'success'
-                  });
-
-                  msg.payload = Buffer.from(res.data);
-
-                  node.send(msg);
-              });
-          });
-
+          node.send(msg);
+        });
       });
+      // }
+    });
   }
 
-  RED.nodes.registerType('google-apis-authentication', GoogleAPIsAuthenticationNode);
+  RED.nodes.registerType(
+    'google-apis-authentication',
+    GoogleAPIsAuthenticationNode
+  );
   RED.nodes.registerType('google-apis', GoogleAPIsNode);
-
 };
